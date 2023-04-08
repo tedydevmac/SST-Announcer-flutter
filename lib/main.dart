abb6e42bfe56bc601e57917910d850413ec4f758
luke yeo <87899265+lukeskywalker22@users.noreply.github.com>
2023-04-08 16:07:25 +0800

    Added skeleton loading features

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,7 @@ class MyApp extends StatelessWidget {
       theme: lightTheme,
       darkTheme: darkTheme,
       home: HomePage(title: 'All announcements'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
