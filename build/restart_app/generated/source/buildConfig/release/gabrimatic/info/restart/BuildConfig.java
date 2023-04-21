61593734af8a00f69b2ab43c1e20a98d22ea1d05
luke yeo <87899265+lukeskywalker22@users.noreply.github.com>
2023-04-21 10:32:14 +0800

    fixed cache issues

---
+++ build/restart_app/generated/source/buildConfig/release/gabrimatic/info/restart/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package gabrimatic.info.restart;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "gabrimatic.info.restart";
+  public static final String BUILD_TYPE = "release";
+}
